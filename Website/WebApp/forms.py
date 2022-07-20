736817b79a4220f64c7a41c09dd324ce28962849
u <adams.din@bk.ru>
2022-07-20 19:35:37 +0300

    Com

---
+++ Website/WebApp/forms.py
@@ -0,0 +1,8 @@
+from .models import Task
+from django.forms import ModelForm
+
+
+class Taskform(ModelForm):
+    class Meta:
+        model = Task
+        fields = ["title", "description"]
--- Website/WebApp/views.py
+++ Website/WebApp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Task
+from .forms import Taskform
 
 
 def index(request):
@@ -9,10 +10,13 @@ def index(request):
 
 def cont(request):
     tasks = Task.objects.all()
+    form = Taskform()
     context = {
-        "tasks": tasks
+        "tasks": tasks,
+        "form": form
     }
     return render(request, 'WebApp/Contacts.html', context)
+
 
 
 
