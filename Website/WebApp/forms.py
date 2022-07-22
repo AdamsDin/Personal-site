eb5e9bbf68436af623f3328dabd524e0a9d2b6a4
u <adams.din@bk.ru>
2022-07-22 12:30:18 +0300

    Com

--- Website/WebApp/forms.py
+++ Website/WebApp/forms.py
@@ -1,8 +1,10 @@
 from .models import Task
-from django.forms import ModelForm
+from django.forms import ModelForm, widgets, TextInput
 
 
 class Taskform(ModelForm):
     class Meta:
         model = Task
         fields = ["title", "description"]
+        widgets = {"title": TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter data'}),
+                   "description": TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter data'})}
